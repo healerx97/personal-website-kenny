e6ce835cbe5ced65fb5855dbc33e926c43010a85
Kenny Yoon <ghun.yoon@gmail.com>
2021-11-19 23:34:17 -0500

    s

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,19 +1,28 @@
 import React from 'react'
-
+import rapoolThumb from '../Assets/RapoolThumb.png'
+import travelHavenThumb from '../Assets/TravelHavenThumb.png'
+import beatMatchThumb from '../Assets/BeatMatchThumb.png'
 function Projects() {
     return (
         <div class='border flex-row divide-y-2 divide-gray-500'>
             <div class='flex p-3 shadow-sm items-center justify-center'>
-                <img class='flex-grow-0 p-10 opacity-90' src= 'https://picsum.photos/300/300'/>
+                <img class='flex-grow-0 p-10 opacity-90 object-contain h-96' src= {beatMatchThumb}/>
                 <div class='flex-row p-10 mx-10 flex-grow border'>
-                    <h2 class='flex text-2xl'>Project Name</h2>
+                    <h2 class='flex text-2xl'>Beat Match</h2>
                     <h3 class='flex text-xl'>Project deets</h3>
                 </div>
             </div>
             <div class='flex p-3 shadow-sm items-center justify-center'>
-                <img class='flex-grow-0 p-10 opacity-90' src= 'https://picsum.photos/300/300'/>
+                <img class='flex-grow-0 p-10 opacity-90 object-contain h-96' src= {rapoolThumb}/>
                 <div class='flex-row p-10 mx-10 flex-grow border'>
-                    <h2 class='flex text-2xl'>Project Name</h2>
+                    <h2 class='flex text-2xl'>Rapool</h2>
+                    <h3 class='flex text-xl'>Project deets</h3>
+                </div>
+            </div>
+            <div class='flex p-3 shadow-sm items-center justify-center'>
+                <img class='flex-grow-0 p-10 opacity-90 object-contain h-96' src= {travelHavenThumb}/>
+                <div class='flex-row p-10 mx-10 flex-grow border'>
+                    <h2 class='flex text-2xl'>Travel Haven</h2>
                     <h3 class='flex text-xl'>Project deets</h3>
                 </div>
             </div>
